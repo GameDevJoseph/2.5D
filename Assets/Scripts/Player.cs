7aa0783e40374826dca6e3da5b8608e833597bdc
IntrovertZombie <117630612+IntrovertZombie@users.noreply.github.com>
2023-07-25 15:47:15 -0400

    Added Character Controller With Double Jumps

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _speed = 5f;
+    [SerializeField] float _gravity = 1.0f;
+    [SerializeField] float _jumpHeight = 15f;
+    CharacterController _controller;
+    float _yVelocity;
+    bool _canDoubleJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontal,0,0);
+        Vector3 velocity = direction * _speed;
+
+        if(_controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(_canDoubleJump)
+                {
+                    _yVelocity += _jumpHeight;
+                    _canDoubleJump = false;
+                }
+            }
+            _yVelocity -= _gravity;
+        }
+        
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
